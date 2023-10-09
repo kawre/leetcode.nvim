e99243dfdfff8a13e89512660c82181b18c49389
kawre <kenterq@gmail.com>
2023-10-09 08:05:26 +0200

    fix(logger): set `nvim-notify` as default notify function when present

--- lua/leetcode/logger/init.lua
+++ lua/leetcode/logger/init.lua
@@ -1,4 +1,5 @@
-local noti_ok, _ = pcall(require, "notify")
+local n_ok, notify = pcall(require, "notify")
+if n_ok then vim.notify = notify end
 
 local config = require("leetcode.config")
 local levels = vim.log.levels
@@ -27,7 +28,7 @@ logger.error = function(msg) logger.log(msg, levels.ERROR) end
 
 ---@param msg msg
 logger.debug = function(msg)
-    if not (config.debug and noti_ok) then return end
+    if not (config.debug and n_ok) then return end
     logger.log({ msg = msg, trace = debug.traceback() }, levels.DEBUG)
 end
 
