d28db5960a6a7a20874233c58f235aab266977a6
kawre <kenterq@gmail.com>
2023-10-16 23:08:01 +0200

    docs(recipes): proper lazy loading example

--- lua/leetcode/parser/utils.lua
+++ lua/leetcode/parser/utils.lua
@@ -6,30 +6,35 @@ local utils = {}
 local entities = {
     ["&lt;"] = "<",
     ["&gt;"] = ">",
+    ["&sum;"] = "∑",
+    ["&prod;"] = "∏",
+    ["&radic;"] = "√",
+    ["&div;"] = "÷",
+    ["&le;"] = "<=",
+    ["&ge;"] = ">=",
+    ["&equals;"] = "=",
     ["&ne;"] = "!=",
     ["&minus;"] = "-",
     ["&plus;"] = "+",
     ["&plusmn;"] = "±",
     ["&cup;"] = "∪",
     ["&times;"] = "×",
-
-    ["&nbsp;"] = " ",
     ["&quot;"] = "\"",
     ["&#39;"] = "'",
     ["&rarr;"] = "",
     ["&larr;"] = "",
-    ["&thinsp;"] = " ",
     ["&hellip;"] = "…",
     ["&lfloor;"] = "⌊",
     ["&rfloor;"] = "⌋",
     ["&amp;"] = "&",
     ["&infin;"] = "∞",
-
     ["&rdquo;"] = "”",
     ["&rsquo;"] = "’",
     ["&ldquo;"] = "“",
     ["&lsquo;"] = "‘",
 
+    ["&thinsp;"] = " ",
+    ["&nbsp;"] = " ",
     ["&lcpad;"] = "",
     ["&lcnl;"] = "",
     ["&lctab;"] = "\t",
@@ -66,7 +71,6 @@ local highlights = {
 function utils.entity(entity)
     if entities[entity] then
         return entities[entity]
-        -- return entity
     else
         log.error("unknown enitity: " .. entity)
         return entity
@@ -76,17 +80,17 @@ end
 ---@param tags string[]
 ---@return string
 function utils.hl(tags)
-    if vim.tbl_isempty(tags) then return "" end
+    if vim.tbl_isempty(tags) then return "leetcode_normal" end
 
     local name = "leetcode_dyn_" .. table.concat(tags, "_")
     if _Lc_dyn_hl[name] then return name end
 
     local default = require("leetcode.theme.default").get()
-
     local theme = default["normal"]
+
     for _, tag in ipairs(tags) do
         local hl = highlights[tag]
-        if hl then theme = vim.tbl_deep_extend("force", theme, default[hl]) end
+        if hl then theme = vim.tbl_extend("force", theme, default[hl]) end
     end
 
     if pcall(vim.api.nvim_set_hl, 0, name, theme) then
