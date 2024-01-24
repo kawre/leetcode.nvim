359595cca152cfaee614cb706405fdbe23558e64
Will Hopkins <willothyh@gmail.com>
2024-01-24 10:37:33 -0800

    fix: crash when unmounting menu

--- lua/leetcode-ui/renderer/menu.lua
+++ lua/leetcode-ui/renderer/menu.lua
@@ -110,13 +110,21 @@ end
 
 function Menu:_unmount()
     for _, tabp in ipairs(require("leetcode.utils").question_tabs()) do
+        vim.api.nvim_set_current_tabpage(tabp.tabpage) -- question tabpage
+        vim.cmd.tabclose()
         if vim.api.nvim_buf_is_valid(tabp.question.bufnr) then
             vim.api.nvim_buf_delete(tabp.question.bufnr, { force = true })
         end
     end
 
-    pcall(vim.api.nvim_buf_delete, self.bufnr, { force = true })
-    pcall(vim.api.nvim_win_close, self.winid, true)
+    -- close menu tabpage
+    if self.bufnr and vim.api.nvim_buf_is_valid(self.bufnr) then
+        vim.api.nvim_buf_delete(self.bufnr, { force = true })
+    end
+
+    if self.winid and vim.api.nvim_win_is_valid(self.winid) then
+        vim.api.nvim_win_close(self.winid, true)
+    end
 
     self = nil
 end
