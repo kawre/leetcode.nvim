fa4da78fbef3199404d1e4e0437bc25bdc78698e
kawre <kenterq@gmail.com>
2024-02-26 22:54:47 +0100

    fix: crash on `Leet exit`

--- lua/leetcode-ui/renderer/menu.lua
+++ lua/leetcode-ui/renderer/menu.lua
@@ -140,10 +140,9 @@ end
 function Menu:_unmount()
     require("leetcode.command").q_close_all()
 
-    -- close menu tabpage
-    if self.bufnr and vim.api.nvim_buf_is_valid(self.bufnr) then
-        vim.api.nvim_buf_delete(self.bufnr, { force = true })
-    end
+    -- if self.bufnr and vim.api.nvim_buf_is_valid(self.bufnr) then
+    --     vim.api.nvim_buf_delete(self.bufnr, { force = true })
+    -- end
 
     if self.winid and vim.api.nvim_win_is_valid(self.winid) then
         vim.api.nvim_win_close(self.winid, true)
