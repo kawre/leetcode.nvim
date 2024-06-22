a35e63daed41ef6bded38c58fb48d66b85d58d3b
Marc Jakobi <marc.jakobi@tiko.energy>
2024-06-23 01:36:35 +0200

    fix: remove nvim-treesitter dependency
    This plugin actually uses Neovim's core API and doesn't
    depend on nvim-treesitter's legacy API (which will be removed).
    
    I have
    
    - Replaced the only place where nvim-treesitter is needed
      to use Neovim's core API to check for a parser on the runtimepath.
    - Updated the reamde to clarify that this plugin doesn't depend
      on nvim-treesitter (there are multiple ways to install parsers).

--- lua/leetcode/parser/init.lua
+++ lua/leetcode/parser/init.lua
@@ -10,12 +10,7 @@ local Parser = Object("LeetParser")
 ---@param text string
 ---@return lc.ui.Group
 function Parser.static:parse(text)
-    local check_for_html = function()
-        local parsers = require("nvim-treesitter.parsers")
-        assert(parsers.get_parser_configs()["html"])
-    end
-
-    if pcall(check_for_html) then
+    if #vim.api.nvim_get_runtime_file("parser/html.so", true) > 0 then
         return Tag:parse(text)
     else
         return Plain:parse(text)
