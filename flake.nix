402943af5d4db1e47650a5430a17c180350019c8
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-06-26 22:29:48 -0500

    feat: nix flake dev environment
    Added nix flake dev shell for dev environment configuration.
    Added stylua to the dev environment.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "leetcode.nvim dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib;
+    eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs;[ stylua ];
+        };
+      }
+    );
+}
